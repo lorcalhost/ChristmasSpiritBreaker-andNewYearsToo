79dd25ec6356fb06387b375368b4eca868da09df
Lorenzo Callegari <Callegari.lorenzo@gmail.com>
2018-12-03 18:00:59 +0100

    Initial commit, now works on WhatsApp
    Facebook Messenger version coming soon

---
+++ WA-christmasSpiritBreaker.py
@@ -0,0 +1,105 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+import sys
+import datetime
+import random
+
+#GENERAL SETUP
+christmasModeEnabled = True #Change to False to disable Christmas mode
+newYearsModeEnabled = True #Change to False to disable New Year's mode
+
+#CHRISTMAS SETUP
+christmas_messages = ["Merry christmas!", "I wish you a merry christmas", "Merry christmas to you and family", "I hate christmas but merry christmas either way"]
+christmas_time_interval = ["08:00", "23:59"]
+christmas_contacts_list = ["John McAfee", "Tim Cook", "Elon Musk", "Person I Hate"]
+
+#NEW YEAR'S SETUP
+newYears_messages = ["Merry christmas!", "I wish you a merry christmas", "Merry christmas to you and family"]
+newYears_time_interval = ["00:00", "00:15"]
+newYears_contacts_list = ["John McAfee", "Tim Cook", "Elon Musk", "Person I Hate"]
+
+#REAL PROGRAM BEGINS, NO MORE SETUP FROM HERE ON
+print("A popup view of WhatsApp web will now open,\nScan the QR code in the page via your app\nDon't close the popup")
+time.sleep(5)
+
+driver = webdriver.Chrome('./chromedriver')
+driver.get("https://web.whatsapp.com/")
+wait = WebDriverWait(driver, 600)
+
+def chrismasMessage():
+    #Sending Christmas message
+    global driver
+    for i in range(0, len(christmas_contacts_list)):
+        msg = random.choice(christmas_messages)
+        x_arg = '//span[contains(@title,' + christmas_contacts_list[i] + ')]'
+        group_title = wait.until(EC.presence_of_element_located((By.XPATH, x_arg)))
+        group_title.click()
+        message = driver.find_elements_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')[0]
+        message.send_keys(msg) #Sends pre selected random message
+        sendbutton = driver.find_elements_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button')[0]
+        sendbutton.click() #Presses send button
+        time.sleep(0.5)
+    return
+
+def newYearsMessage():
+    #Sending New Year's message
+    global driver
+    for i in range(0, len(newYears_contacts_list)):
+        msg = random.choice(newYears_messages)
+        x_arg = '//span[contains(@title,' + christmas_contacts_list[i] + ')]'
+        group_title = wait.until(EC.presence_of_element_located((By.XPATH, x_arg)))
+        group_title.click()
+        message = driver.find_elements_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')[0]
+        message.send_keys(msg) #Sends pre selected random message
+        sendbutton = driver.find_elements_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button')[0]
+        sendbutton.click() #Presses send button
+        time.sleep(0.5) #Waits half a second before sending the next message
+    return
+
+def newRandTime(customTimeInterval):
+    randTimeHour = random.randint(int(customTimeInterval[0][0:2]), int(customTimeInterval[1][0:2]))
+    if int(randTimeHour) == int(customTimeInterval[1][0:2]):
+        randTimeMinute = random.randint(0, int(customTimeInterval[1][3:5]))
+    elif int(randTimeHour) == int(customTimeInterval[0][0:2]):
+        randTimeMinute = random.randint(int(customTimeInterval[1][3:5]), 59)
+    else:
+        randTimeMinute = random.randint(0, 59)
+    return str(str(randTimeHour).zfill(2) + ":" + str(randTimeMinute).zfill(2))
+
+christmas_contacts_list_times = [[0]*2 for i in range(len(christmas_contacts_list))]
+newYears_contacts_list_times = [[0]*2 for i in range(len(christmas_contacts_list))]
+
+
+for i in range(0, len(christmas_contacts_list)):
+    christmas_contacts_list[i] = str('"' + christmas_contacts_list[i] + '"')
+    newTime = newRandTime(christmas_time_interval)
+    christmas_contacts_list_times[i][0] = int(newTime[0:2])
+    christmas_contacts_list_times[i][1] = int(newTime[3:5])
+
+for i in range(0, len(newYears_contacts_list)):
+    newYears_contacts_list[i] = str('"' + newYears_contacts_list[i] + '"')
+    newTime = newRandTime(christmas_time_interval)
+    newYears_contacts_list_times[i][0] = int(newTime[0:2])
+    newYears_contacts_list_times[i][1] = int(newTime[3:5])
+
+while True:
+    if (christmasModeEnabled):
+        if (int(datetime.datetime.today().day) == 25) and (int(datetime.datetime.today().month) == 12):
+            for i in range(0, len(christmas_contacts_list)):
+                if (int(datetime.datetime.today().hour) in christmas_contacts_list_times[0]):
+                    if (int(datetime.datetime.today().minute) in christmas_contacts_list_times[1]):
+                        chrismasMessage()
+    if (newYearsModeEnabled):
+        if (int(datetime.datetime.today().day) == 1) and (int(datetime.datetime.today().month) == 1):
+            for i in range(0, len(newYears_contacts_list)):
+                if (int(datetime.datetime.today().hour) in newYears_contacts_list_times[0]):
+                    if (int(datetime.datetime.today().minute) in newYears_contacts_list_times[1]):
+                        newYearsMessage()
+    
+
+                
+    time.sleep(60) #Wait one minute to check if it's #morningtime
